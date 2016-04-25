65b3d98ec4e679b77a9fbc76fd950f0df8fb0ba2
Anderson Juhasc <anderson.juhasc@gmail.com>
2016-04-25 20:25:09 -0300

    fix methods

--- src/Rest.php
+++ src/Rest.php
@@ -1,14 +1,15 @@
 <?php
 
-//namespace Blinktrade;
+namespace Blinktrade;
 
 /**
  * Rest
  *
- * @param {number} $broker_id
- * @param {string} $key
- * @param {string} $secret
+ * @param {number}  $broker_id
+ * @param {string}  $key
+ * @param {string}  $secret
  * @param {boolean} $env
+ * @param {string}  $version
  */
 class Rest
 {
@@ -33,7 +34,7 @@ class Rest
         $this->version = $version;
     }
 
-    private function send($array)
+    public function send($array)
     {
         $key = $this->key;
         $secret = $this->secret;
@@ -53,10 +54,10 @@ class Rest
         curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($ch, CURLOPT_HTTPHEADER, [
-            'Nonce: ' . $nonce,                             # The nonce must be an integer, always greater than the previous one.
-            'APIKey: ' . $key,                              # Your APIKey
+            'Nonce: ' . $nonce,                       # The nonce must be an integer, always greater than the previous one.
+            'APIKey: ' . $key,                        # Your APIKey
             'Content-Type: application/json',         # You must POST a JSON message
-            'Signature: ' . $signature,                      # Use the API Secret  to sign the nonce using HMAC_SHA256 algo
+            'Signature: ' . $signature,               # Use the API Secret  to sign the nonce using HMAC_SHA256 algo
             'user-agent: blinktrade_tools/0.1',
         ]);
 
@@ -75,39 +76,45 @@ class Rest
         $this->send($array);
     }
 
-    public function openOrders()
+    public function openOrders($page = 0, $page_size = 100)
     {
+        $client_order_id = strval(time());
+
         $array = [
             "MsgType" => "U4",
-            "OrdersReqID" => 1,
-            "Page" => 0,
-            "PageSize" => 100,
+            "OrdersReqID" => $client_order_id,
+            "Page" => $page,
+            "PageSize" => $page_size,
             "Filter" => ["has_leaves_qty eq 1"],
         ];
 
         $this->send($array);
     }
 
-    public function executedOrders()
+    public function executedOrders($page = 0, $page_size = 100)
     {
+        $client_order_id = strval(time());
+
         $array = [
             "MsgType" => "U4",
-            "OrdersReqID" => 1,
-            "Page" => 0,
-            "PageSize" => 100,
+            "OrdersReqID" => $client_order_id,
+            "Page" => $page,
+            "PageSize" => $page_size,
             "Filter" => ["has_cum_qty eq 1"],
         ];
 
         $this->send($array);
     }
 
-    public function cancelledOrders()
+    public function cancelledOrders($page = 0, $page_size = 100)
     {
+        $client_order_id = strval(time());
+
         $array = [
             "MsgType" => "U4",
-            "OrdersReqID" => 1,
-            "Page" => 0,
-            "PageSize" => 100,
+            "OrdersReqID" => $client_order_id,
+            "Page" => $page,
+            "PageSize" => $page_size,
             "Filter" => ["has_cxl_qty eq 1"],        ];
 
         $this->send($array);
@@ -121,11 +128,11 @@ class Rest
         $array = [
             "MsgType" => "D",              # New Order Single message. Check for a full doc here: http://www.onixs.biz/fix-dictionary/4.4/msgType_D_68.html
             "ClOrdID" => $client_order_id, # Unique identifier for Order as assigned by you
-            "Symbol" => $symbol,          # Can be BTCBRL, BTCPKR, BTCVND, BTCVEF, BTCCLP.
-            "Side" => $side,                 # 1 - Buy, 2-Sell
-            "OrdType" => $orderType,              # 2 - Limited order
-            "Price" => $price,        # Price in satoshis
-            "OrderQty" => $orderQty,         # Qty in saothis
+            "Symbol" => $symbol,           # Can be BTCBRL, BTCPKR, BTCVND, BTCVEF, BTCCLP.
+            "Side" => $side,               # 1 - Buy, 2-Sell
+            "OrdType" => $orderType,       # 2 - Limited order
+            "Price" => $price,             # Price in satoshis
+            "OrderQty" => $orderQty,       # Qty in saothis
             "BrokerID" => $broker_id       # 1=SurBitcoin, 3=VBTC, 4=FoxBit, 5=Tests , 8=UrduBit, 9=ChileBit
         ];
 
@@ -148,26 +155,26 @@ class Rest
         $client_id = strval(time());
 
         $array = [
-            "MsgType" => "U18",    # Deposit request
+            "MsgType" => "U18",             # Deposit request
             "DepositReqID" => $client_id,   # Deposit Request ID.
-            "Currency" => "BTC",   # Currency.
-            "BrokerID" => $broker_id       # Exchange ID
+            "Currency" => "BTC",            # Currency.
+            "BrokerID" => $broker_id        # Exchange ID
         ];
 
         $this->send($array);
     }
 
-    public function FIATDeposit()
+    public function FIATDeposit($value)
     {
         $broker_id = $this->broker_id;
 
         $array = [
-            "MsgType" => "U18",    # Deposit request
-            "DepositReqID" => 1,   # Deposit Request ID.
+            "MsgType" => "U18",       # Deposit request
+            "DepositReqID" => 1,      # Deposit Request ID.
             "DepositMethodID" => 403, # Deposit Method ID - Check with your exchange.
-            "Value" => 150000000000, # Amount in satoshis
-            "Currency" =>  "BRL",   # Currency.
-            "BrokerID" => $broker_id       # Exchange ID
+            "Value" => $value * 1e8,  # Amount in satoshis
+            "Currency" =>  "BRL",     # Currency.
+            "BrokerID" => $broker_id  # Exchange ID
         ];
 
         $this->send($array);
@@ -179,54 +186,46 @@ class Rest
 
         $array = [
             "MsgType" => 'U6',
-            "WithdrawReqID" => $client_id,    # Request ID.
-            "Method" => 'bitcoin',        # bitcoin for BTC. Check with the exchange all available withdrawal methods
-            "Amount" => $amount,          # Amount in satoshis
-            "Currency" => 'BTC',          # Currency
+            "WithdrawReqID" => $client_id,  # Request ID.
+            "Method" => 'bitcoin',          # bitcoin for BTC. Check with the exchange all available withdrawal methods
+            "Amount" => $amount * 1e8,      # Amount in satoshis
+            "Currency" => 'BTC',            # Currency
             "Data" => [
-                "Wallet" => $address  # Your Wallet
-            ]
-        ];
-
-        $this->send($array);
-    }
-
-    public function FIATWithdrawal($method, $currency, $amount)
-    {
-        $client_id = strval(time());
-
-        $array = [
-            "MsgType" => 'U6',
-            "WithdrawReqID" => $client_id,
-            "Method" => $method,  # Method.
-            "Currency" => $currency,
-            "Amount" => $amount,
-            "Data" => [
-                "AccountBranch" => "111",
-                "AccountNumber" => "4444-5",
-                "AccountType" => "corrente",
-                "CPF_CNPJ" => "00000000000"
+                "Wallet" => $address        # Your Wallet
             ],
         ];
 
         $this->send($array);
     }
 
-    public function listWithdrawals()
+    public function FIATWithdrawal($method, $currency, $amount, $data = [])
+    {
+        $client_id = strval(time());
+
+        $array = [
+            "MsgType" => 'U6',
+            "WithdrawReqID" => $client_id,
+            "Method" => $method,
+            "Currency" => $currency,
+            "Amount" => $amount * 1e8,
+            "Data" => $data,
+        ];
+
+        $this->send($array);
+    }
+
+    public function listWithdrawals($status, $page = 0, $page_size = 100)
     {
         $client_id = strval(time());
 
         $array = [
             'MsgType' => 'U26',
-            'WithdrawListReqID' => $client_id,    # WithdrawList Request ID
-            'Page' => 0,
-            'PageSize' => 100,
-            'StatusList' => ['1', '2']   # 1-Pending, 2-In Progress, 4-Completed, 8-Cancelled
+            'WithdrawListReqID' => $client_id,  # WithdrawList Request ID
+            "Page" => $page,
+            "PageSize" => $page_size,
+            'StatusList' => $status             # example: ['1', '2'], 1-Pending, 2-In Progress, 4-Completed, 8-Cancelled
         ];
 
         $this->send($array);
     }
 }
-
-$test = new Rest('5', 'P14vYNUoggIJvio1esyDpragE9b1t7XgZ2CiNP2Yf4c', 'MJGRyshogq7YElojJLOdZUr4mTmTypekXFAmFFKxldY', true);
-$test->listWithdrawals();
